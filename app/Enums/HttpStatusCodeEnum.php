4bba801309d8a960f725c2edcdadb5c496165ebc
Vinicius <viniciushenrique.dev@gmail.com>
2026-06-08 17:12:15 -0300

    feat(app): create enum for http status code

---
+++ app/Enums/HttpStatusCodeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums\;
+
+enum HttpStatusCodeEnum: int
+{
+    case SUCCESS_OK = 200;
+    case SUCCESS_CREATED = 201;
+    case CLIENT_ERROR_UNAUTHORIZED = 401;
+    case CLIENT_ERROR_NOT_FOUND = 404;
+    case SERVER_ERROR_INTERNAL = 500;
+}
