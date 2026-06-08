45dfe40127a25de58d14f2a5a071ae706cf2c1cc
Vinicius <viniciushenrique.dev@gmail.com>
2026-06-08 15:47:11 -0300

    feat(app): configure HasApiTokens in model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,13 +12,14 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password', 'is_active', 'last_login_at'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, HasUuids, SoftDeletes;
+    use HasFactory, Notifiable, HasUuids, SoftDeletes, HasApiTokens;
 
     /**
      * Get the attributes that should be cast.
