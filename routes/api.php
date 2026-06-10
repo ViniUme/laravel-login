4c9d5b12bab390f5de08a08fa8f4b8cd7468a3bf
Vinicius <viniciushenrique.dev@gmail.com>
2026-06-10 17:44:15 -0300

    feat: creante and configure sign in feature

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\SignUpAuthV1ApiController;
+use App\Http\Controllers\Api\V1\Auth\SignInAuthV1ApiController;
 
 Route::prefix('v1')->group(function() {
     Route::prefix('auth')->group(function() {
         Route::post('sign-up', SignUpAuthV1ApiController::class)->name('auth.sign-up');
+        Route::post('sign-in', SignInAuthV1ApiController::class)->middleware('throttle:5,1')->name('auth.sign-in');
+    });
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
     });
 });
