e9fd0cd024d9f1b7e1a61887cf7c903fefa8f653
Vinicius <viniciushenrique.dev@gmail.com>
2026-06-11 14:05:05 -0300

    refactor(routes): change sign in and sign up route name

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\Api\V1\Auth\SignInAuthV1ApiController;
 
 Route::prefix('v1')->group(function() {
     Route::prefix('auth')->group(function() {
-        Route::post('sign-up', SignUpAuthV1ApiController::class)->name('auth.sign-up');
-        Route::post('sign-in', SignInAuthV1ApiController::class)->middleware('throttle:5,1')->name('auth.sign-in');
+        Route::post('sign-up', SignUpAuthV1ApiController::class)->name('api.v1.auth.sign-up');
+        Route::post('sign-in', SignInAuthV1ApiController::class)->middleware('throttle:5,1')->name('api.v1.auth.sign-in');
     });
 
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
