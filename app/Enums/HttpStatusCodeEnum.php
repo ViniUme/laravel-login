75ed70ed78792a033153619f67edac67907c2c68
Vinicius <viniciushenrique.dev@gmail.com>
2026-06-11 13:14:16 -0300

    refactor(app): add comments to separate codes by category in HttpStatusCodeEnum

--- app/Enums/HttpStatusCodeEnum.php
+++ app/Enums/HttpStatusCodeEnum.php
@@ -4,13 +4,18 @@ namespace App\Enums;
 
 enum HttpStatusCodeEnum: int
 {
+    // * Success codes
     case SUCCESS_OK = 200;
     case SUCCESS_CREATED = 201;
+
+    // * Client error codes
     case CLIENT_ERROR_BAD_REQUEST = 400;
     case CLIENT_ERROR_UNAUTHORIZED = 401;
     case CLIENT_ERROR_FORBIDDEN = 403;
     case CLIENT_ERROR_NOT_FOUND = 404;
     case CLIENT_ERROR_UNPROCESSABLE_ENTITY = 422;
     case CLIENT_ERROR_TOO_MANY_REQUESTS = 429;
+
+    // * Server error code
     case SERVER_ERROR_INTERNAL = 500;
 }
