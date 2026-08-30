edc196a76d5daa42b614fe48ab676b87042234da
ViniUme <viniciushenriquedsantos@gmail.com>
2026-08-30 12:28:10 -0300

    refactor: delete sign in page

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use App\Http\Controllers\Auth\SignInAuthController;
 
 Route::inertia('/', 'Welcome')->name('home');
 
-
-Route::prefix('auth')->group(function () {
-    Route::get('/sign-in', SignInAuthController::class)->name('web.auth.sign-in');
-});
-
