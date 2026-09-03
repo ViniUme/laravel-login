2e1551f560a7ca186e3963464af4e7fb25bb00f1
Vinicius <viniciushenriquedsantos@gmail.com>
2026-09-03 19:24:25 +0000

    refactor(web): replace design of sign up page

--- resources/js/pages/Auth/SignUpAuth.vue
+++ resources/js/pages/Auth/SignUpAuth.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import SignupForm from "@/components/signup-03/components/SignupForm.vue";
 import ModeToggle from "@/components/ui/button/ModeToggle.vue";
+import { GalleryVerticalEnd } from "@lucide/vue"
 </script>
 
 <template>
-    <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-        <div class="w-full max-w-sm">
+    <div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div class="flex w-full max-w-sm flex-col gap-6">
+            <a href="#" class="flex items-center gap-2 self-center font-medium">
+                <div class="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                    <GalleryVerticalEnd class="size-4" />
+                </div>
+                Laravel Login
+            </a>
             <SignupForm />
         </div>
-        <div class="size-22 fixed right-0 bottom-0">
+        <div class="fixed right-0 bottom-0 size-22">
             <ModeToggle />
         </div>
     </div>
