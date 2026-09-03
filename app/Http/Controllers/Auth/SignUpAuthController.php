4329a20c3ea254878623d30867fb3dc3a902e0ac
Vinicius <viniciushenriquedsantos@gmail.com>
2026-09-03 16:55:56 +0000

    feat: configure inertia to render sign up page

--- app/Http/Controllers/Auth/SignUpAuthController.php
+++ app/Http/Controllers/Auth/SignUpAuthController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class SignUpAuthController extends Controller
 {
-    //
+    public function __invoke(): Response
+    {
+        return Inertia::render('Auth/SignUpAuth');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Contrllers\Auth\SignUpAuthController;
+use App\Http\Controllers\Auth\SignUpAuthController;
 
 Route::inertia('/', 'Welcome')->name('home');
 
