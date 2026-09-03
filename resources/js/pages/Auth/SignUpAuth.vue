99039763acb0e83bbec342cec5eeab64d3992f98
Vinicius <viniciushenriquedsantos@gmail.com>
2026-09-03 18:22:23 +0000

    feat: install and configure sign up page design

--- resources/js/pages/Auth/SignUpAuth.vue
+++ resources/js/pages/Auth/SignUpAuth.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import SignupForm from "@/components/signup-03/components/SignupForm.vue";
 </script>
 
 <template>
+    <div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+        <div class="w-full max-w-sm">
+            <SignupForm />
+        </div>
+    </div>
 </template>
 
 <style>
